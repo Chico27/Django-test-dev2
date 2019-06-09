a1ddf0a9b4a051ba5821df99f7e6b4203fb523e1
Chico27 <masamasa.987654321@gmail.com>
2019-06-09 11:09:05 +0900

    add contact page

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+
