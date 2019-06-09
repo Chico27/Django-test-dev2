2515425415fccfa4ae3a8c145c39e8ff52aa7c3f
Chico27 <masamasa.987654321@gmail.com>
2019-06-09 16:37:24 +0900

    page calculate

--- venv/src/pages/views.py
+++ venv/src/pages/views.py
@@ -15,6 +15,7 @@ def about_view(request, *args, **kwargs):
     my_context = {
         "my_text": "This is about us",
         "my_number": 123,
+        "this_is_true": True,
         "my_list": [123, 4324, 34243]
     }
     return render(request, "about.html", my_context)
