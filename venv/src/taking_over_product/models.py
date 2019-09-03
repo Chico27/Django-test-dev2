352e5de11bc654225bce0339c417a21fd553d455
chico27 <masamasa.987654321@gmail.com>
2019-09-03 19:56:53 +0900

    new

--- venv/src/taking_over_product/models.py
+++ venv/src/taking_over_product/models.py
@@ -4,4 +4,5 @@ from products.models import Product
 # Create your models here.
 class TakingOverProduct(Product):
     taking_over_description = models.TextField(blank=True, null=True)
+    limit_price = models.DecimalField(decimal_places=2, max_digits=1000)
 
