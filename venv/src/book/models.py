56cfb454611b1b5f38b5b9f38678de329afba58e
chico27 <masamasa.987654321@gmail.com>
2019-12-31 16:01:56 +0900

    new

--- venv/src/book/models.py
+++ venv/src/book/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=120) # max_length=required
     author = models.CharField(max_length=120) # max_length=required
+    price = models.DecimalField(decimal_places=2, max_digits=100)
 
 
