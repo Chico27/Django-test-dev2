26dbf40ca081e3cc97c5e40143fdb8514c86ce3b
Chico27 <masamasa.987654321@gmail.com>
2021-09-05 14:53:18 +0900

    commit.

--- venv/src/book/admin.py
+++ venv/src/book/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Book
+from .models import Author, Book
 
+admin.site.register(Author)
 admin.site.register(Book)
